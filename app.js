b57af0be25d351b9a727f7f4a5db7afb47e415b3
suisui xia <xiasuisui0402@gmail.com>
2023-06-11 23:47:57 -0700

    add cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
 );
 app.use(cors({
    credentials: true,
-   origin: "http://localhost:3000",
+   origin: ["http://localhost:3000", "https://a5--adorable-fairy-cc497a.netlify.app"]
  })
 )
 app.use(express.json());
